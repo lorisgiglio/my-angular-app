e377593ce6b17fb7e03373819212c932f458aacf
Loris Giglio <loris.giglio@gmail.com>
2025-02-05 14:56:51 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ReaderComponent } from './reader/reader.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent }, // Main page
+    { path: 'reader', component: ReaderComponent }, // Reader page
+  ];
---
+++ src/app/reader/reader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reader',
+  imports: [],
+  templateUrl: './reader.component.html',
+  styleUrl: './reader.component.css'
+})
+export class ReaderComponent {
+
+}
